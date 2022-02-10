2f9991e2c9bac94fbd11a3cf313aeaac7575daf8
jordi <jordi.lanau@yahoo.com>
2022-02-10 22:44:58 +0000

    Update seeds

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -2,59 +2,29 @@ const { Comment } = require('../models');
 
 const commentData = [
   {
-    content: 'so proud of you!',
-    user_id: 1,
+    content: 'I totally agree!',
+    user_id: 5,
     post_id: 1,
   },
   {
-    content: 'yeah!!!! so proud of you!',
-    user_id: 2,
-    post_id: 1,
+    content: 'Fair point',
+    user_id: 4,
+    post_id: 3,
   },
   {
-    content: 'well done little man!',
+    content: 'Definitely!',
     user_id: 3,
     post_id: 2,
   },
   {
-    content: 'hey jess, good to see you here!',
+    content: 'Learning never ends!',
     user_id: 2,
-    post_id: 3,
-  },
-  {
-    content: 'nice post!',
-    user_id: 3,
     post_id: 4,
   },
   {
-    content: 'yes you are!',
-    user_id: 4,
+    content: 'nice post!',
+    user_id: 1,
     post_id: 5,
-  },
-  {
-    content: "I don't know!!",
-    user_id: 5,
-    post_id: 6,
-  },
-  {
-    content: 'yeah not really...',
-    user_id: 4,
-    post_id: 7,
-  },
-  {
-    content: 'do you need to ask?',
-    user_id: 3,
-    post_id: 8,
-  },
-  {
-    content: 'getting old? :O',
-    user_id: 2,
-    post_id: 9,
-  },
-  {
-    content: 'seriously?',
-    user_id: 4,
-    post_id: 10,
   },
 ];
 
--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -2,53 +2,33 @@ const { Post } = require('../models');
 
 const postData = [
   {
-    title: 'This is cranky_pants first post',
-    content: 'cranky_pants has written his first blog post!',
+    title: 'I love coding',
+    content:
+      'Non labore laboris fugiat ullamco exercitation quis consectetur sit irure culpa fugiat. Exercitation dolore non ipsum fugiat eiusmod pariatur ipsum enim esse mollit laboris irure. Sunt consequat culpa sint deserunt deserunt ea et cupidatat ad eiusmod elit adipisicing. Dolor sint quis incididunt amet voluptate. Nostrud ipsum ullamco aute esse nostrud sint qui id eu anim id amet sint.',
     user_id: 1,
   },
   {
-    title: 'This is cranky_pants second post',
-    content: 'Another blog post by cranky_pants!',
-    user_id: 1,
-  },
-  {
-    title: "jess's first post",
-    content: 'I like to write!!!',
+    title: "A developer's job is never done!",
+    content:
+      'Amet incididunt reprehenderit commodo ea incididunt et qui deserunt velit cupidatat in. Cupidatat Lorem cillum velit quis sunt adipisicing ad incididunt ex ea aute et cillum. Aliquip irure reprehenderit id anim. Tempor pariatur in exercitation magna duis dolor voluptate eu. Nostrud velit id ea sunt.',
     user_id: 2,
   },
   {
-    title: 'another post (by jess)',
-    content: 'I did say I like to write!',
-    user_id: 2,
-  },
-  {
-    title: 'First post by little_john',
-    content: 'getting there!',
+    title: 'Remember to code every day',
+    content:
+      'Incididunt cupidatat qui ad dolor eiusmod. Adipisicing commodo eiusmod labore nisi deserunt exercitation irure quis irure proident aliquip quis quis. Dolor eu ullamco ut mollit.',
     user_id: 3,
   },
   {
-    title: 'little_john is on a roll!',
-    content: 'yeah baby!',
-    user_id: 3,
-  },
-  {
-    title: "I'm paul and I'm cool",
-    content: 'or so I think...',
+    title: 'I need to learn advanced data structures',
+    content:
+      'Nisi ex consectetur incididunt quis duis Lorem elit labore eiusmod adipisicing quis id nisi. Ut et occaecat do sunt aliquip quis irure in. Sunt reprehenderit veniam ut non adipisicing proident esse amet aliqua. Veniam id ea tempor nostrud laborum dolore dolor do veniam incididunt. Deserunt ex eiusmod velit elit elit duis proident velit occaecat veniam sit duis voluptate. Deserunt tempor ex culpa cupidatat nisi ullamco sunt ea.',
     user_id: 4,
   },
   {
-    title: 'another post by the wannabe cool guy',
-    content: "why doesn't anyone think I'm cool!!?",
-    user_id: 4,
-  },
-  {
-    title: 'I think I was in the spice girls',
-    content: "or maybe not, I don't remember anything anymore...",
-    user_id: 5,
-  },
-  {
-    title: 'if you wanna be my lover...',
-    content: "I can't believe I forgot the rest of the song!!",
+    title: 'I love learning algorithms',
+    content:
+      'Eu velit voluptate elit eiusmod qui excepteur laboris ut esse velit. Ut consectetur officia excepteur commodo do deserunt cillum sunt aute anim esse. Veniam labore officia id quis quis nulla aliquip anim in ea. Deserunt proident non enim cillum occaecat nostrud.',
     user_id: 5,
   },
 ];
